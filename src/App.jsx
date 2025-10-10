e6d42810c658a796077a07684579bda469a0f1df
Anup Parkhi <anupparkhi@Mac.lan>
2025-10-10 08:53:38 -0500

    Added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Experience from "./components/Experience";
 import Education from "./components/Education";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
-import ChatBox from "./components/Chatbox";
+import ChatBox from "./components/chatbox";
 import HashLoader from "react-spinners/HashLoader";
 
 import { useState, useEffect } from "react";
